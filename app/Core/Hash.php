c721cc0d0cde640117eee6a0a8b063504e0c99b3
JangTaeGyu <ttggbbgg2@gmail.com>
2016-08-04 10:08:11 +0900

    init

--- app/Core/Hash.php
+++ app/Core/Hash.php
@@ -4,9 +4,19 @@ namespace App\Core;
 
 class Hash
 {
+    const COST = 12;
+
     public static function make($string, $salt = '')
     {
         return hash('sha256', $string . $salt);
+    }
+
+    public static function passwordHash($string, $salt = '')
+    {
+        return password_hash($string, PASSWORD_DEFAULT, [
+            'cost' => self::COST,
+            'salt' => $salt,
+        ]);
     }
 
     public static function salt($length)
